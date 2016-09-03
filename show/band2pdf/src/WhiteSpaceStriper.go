1348df51a9ad2a450514f28d94542d64bc27651e
Chen, Chihhan <aguaithefreak@gmail.com>
2016-09-03 19:35:19 +0800

    BarLine should stripped saperated

--- show/band2pdf/src/WhiteSpaceStriper.go
+++ show/band2pdf/src/WhiteSpaceStriper.go
@@ -9,7 +9,7 @@ import (
 // Strip all the unnecessary char in the sting matched
 // Thx StackOverflow
 func WhiteSpaceStriper(str string) string {
-	BarStriped:= strings.Replace(str, "|", "", -1)
+	
 	return strings.Map(func(x rune) rune {	// difficult to decide if I should keep it anonymous?
 		if unicode.IsSpace(x) {
 			return -1
