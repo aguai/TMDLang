3b22ac38f8f576317da89abe9e0925193427efde
Chen, Chihhan <aguaithefreak@gmail.com>
2016-09-27 00:35:37 +0800

    basic elements added

---
+++ src/TMDParser.py
@@ -0,0 +1,10 @@
+import re
+PartContentRegex = '(?P<partname>\\S+?):(?P<InstrumentName>\\S+?)@(\\||\\[)(?P<Timing>\\S+?)(\\||\\])\\{(\\s+?)(?P<PartContent>[^}]+)\\}'
+SongNameRegex = '\\s*\\*\\*\\s+?(?P<SongName>[^\\*]+)\\s+?\\*\\*\\s*'
+InstrumentSet=set()
+PartSet=set()
+
+def FormaterStripper(str):
+    ''' anything for format shall be trimed here'''
+    return str.replace(' ', '').replace('\n', '').replace('|', '').replace('\t', '')
+
