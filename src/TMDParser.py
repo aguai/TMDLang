b5c09c35b2576e548548de81a233dca80ab64379
Chen, Chihhan <aguaithefreak@gmail.com>
2016-10-02 16:45:10 +0800

    add CommitStripper

--- src/TMDParser.py
+++ src/TMDParser.py
@@ -1,9 +1,13 @@
-PartContentRegex = '(?P<partname>\\S+?):(?P<InstrumentName>\\S+?)@(\\||\\[)(?P<Timing>\\S+?)(\\||\\])\\{(\\s+?)(?P<PartContent>[^}]+)\\}'
-SongNameRegex = '\\s*\\*\\*\\s+?(?P<SongName>[^\\*]+)\\s+?\\*\\*\\s*'
+import re
+PartContentRegex = r'(?P<partname>\\S+?):(?P<InstrumentName>\\S+?)@(\\||\\[)(?P<Timing>\\S+?)(\\||\\])\\{(\\s+?)(?P<PartContent>[^}]+)\\}'
+SongNameRegex = r'\\s*\\*\\*\\s+?(?P<SongName>[^\\*]+)\\s+?\\*\\*\\s*'
 InstrumentSet=set()
 PartSet=set()
 
+def CommitStripper(str):
+    return re.sub(r'#.+(\n|\r|\r\n|\n\r|$)', '',str)
+
 def FormaterStripper(str):
     ''' anything for format shall be trimed here'''
-    return str.replace(' ', '').replace('\n', '').replace('|', '').replace('\t', '')
+    return str.replace(' ', '').replace('\n', '').replace('|', '').replace('\t', '').replace('\r', '')
 
