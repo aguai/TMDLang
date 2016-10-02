e5c2de2e2a0d1b3a8ebce69df910281793ec66ef
Chen, Chihhan <aguaithefreak@gmail.com>
2016-10-02 22:53:36 +0800

    add pyc to ignore

--- src/TMDParser.py
+++ src/TMDParser.py
@@ -1,9 +1,11 @@
 import re
-PartContentRegex = r'(?P<partname>\\S+?):(?P<InstrumentName>\\S+?)@(\\||\\[)(?P<Timing>\\S+?)(\\||\\])\\{(\\s+?)(?P<PartContent>[^}]+)\\}'
-SongNameRegex = r'\\s*\\*\\*\\s+?(?P<SongName>[^\\*]+)\\s+?\\*\\*\\s*'
-InstrumentSet=set()
-PartSet=set()
-
+PartContentRegex = '(?P<partname>\\S+?):(?P<InstrumentName>\\S+?)@(\\||\\[)(?P<Timing>\\S+?)(\\||\\])\\{(\\s+?)(?P<PartContent>[^}]+)\\}'
+SongNameRegex = '\\s*\\*\\*\\s+?(?P<SongName>[^\\*]+)\\s+?\\*\\*\\s*'
+InstrumentSet = set()
+PartSet = set()
+ReservedInstrumet = set()
+ReservedInstrumet.add('__CHORD__')
+ReservedInstrumet.add('__GROOVE__')
 def CommitStripper(str):
     return re.sub(r'#.+(\n|\r|\r\n|\n\r|$)', '',str)
 
