3c887447e11ad4a3f6159c6af45cd6b9cae3fb4b
Chen, Chihhan <aguaithefreak@gmail.com>
2016-10-04 19:51:38 +0800

    add som feature

--- src/tmd2pdf.py
+++ src/tmd2pdf.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
+import cairo
 import TMDScanner
 import re
 import sys
+from pathlib import Path
+
 def main():
     if len(sys.argv)!=2:
-        print("usage:\n%s InputFile.pdf\n", sys.argv[0])
-    print(sys.argv[1])
+        print("usage:\n%s InputFile.pdf\n" % sys.argv[0])
+        exit()
+    
+    print("collectting notation info from %s... " % sys.argv[1])
+    
+    if Path(sys.argv[1]).is_file() != True:
+        print("there is no file %s!" % sys.argv[1])
+        exit()
+    
     
 if __name__ == '__main__':
     main()
