78e249672b03e1159502e8c53c0b9033ddecdc24
Chen, Chihhan <aguaithefreak@gmail.com>
2016-10-07 16:20:35 +0800

    add a note(copy from interpretor)

--- src/TMD_Note.py
+++ src/TMD_Note.py
@@ -5,3 +5,15 @@ yy=[]
 for i in range(len(x)):
     yy.append(tuple([x[i].replace('-',''), x[i].count('-')+1]))
 #############
+for i in AllPartContentList:
+    if i[1]=='CHORD':
+        print(i[0])
+        ooxx=re.findall(r"\<(?P<Base>[12348][26]?)\*\>(?P<ChordString>[^<$]+)",i[3])[0]
+        print('\tbase:\t'+ooxx[0])
+        MatchChordWithLength=re.findall(r"(?P<ChordWithLength>\[[1-7][^\]]*\]\-*)", ooxx[1])
+        print('\t\tchords:\n\t\t'+str(MatchChordWithLength))
+        temp=[]
+        for itr in range(len(MatchChordWithLength)):
+            temp.append(MatchChordWithLength[itr].count('-')+1)
+        print('\t\t'+str(temp))
+        print('************************************************************')
