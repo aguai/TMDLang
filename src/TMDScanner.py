aa4553f266a09e7673a77fc2ad6d96176eb18fd8
Chen, Chihhan <aguaithefreak@gmail.com>
2016-11-12 22:23:36 +0800

    typo

--- src/TMDScanner.py
+++ src/TMDScanner.py
@@ -5,7 +5,7 @@ PartSequencePattern = r"\-\>([^\#]+)->\#"
 PartContentPattern = r"(?P<partname>\S+?)?:(?P<InstrumentName>\S+?)?@\[(?P<Timing>\S+?)?\]\{\s+?(?P<PartContent>[^}]+)\}"
 #PartContentPattern = r"(\S*):(\S*)@\[(\S*)\]\{([^\}]+)\}"
 SongNamePattern = r"\s*\*\*\s+?(?P<SongName>[^\*]+)\s+?\*\*\s*"
-TempoPattern = r"\s*?\!\s*?\=s\s*?(\d\d\d?\.?\d?\d?)\s*?\n"
+TempoPattern = r"\s*?\!\s*?\=\s*?(\d\d\d?\.?\d?\d?)\s*?\n"
 KeyPattern = r"\s*?\?\s*\=\s*(?P<Key>[ABCDEFGabcdefg][',]?m?)\s*?\n"
 SignaturePattern = r"^\s*\<((?P<BeatsPerBar>\d\d?)\/(?P<BeatType>[12348][26]?))\>\s*"
 
@@ -79,12 +79,14 @@ def SignatureGetter(inputFile):
         return [int(Sig[1]), int(Sig[2])]
 ########################## Pass 2 ########################################
 
-CHORDStringPattern = r"(?P<Chord>\[[1-7][^\]]*\]\-*)"
-CHORDRootAndQualityPattern = r"(?P<Root>[1-7]['|,]?)(?P<Quality>[^\]]*)"
+
 
 
 def ChordStringGetter(PartsContainsChord):    
     CHORDPartStringPattern = r"\<(?P<Base>[12348][26]?)\*\>(?P<ChordString>[^<$]+)"   # return a tuple ('base', ''StringWithChords)
+    CHORDStringPattern = r"(?P<Chord>\[[1-7][^\]]*\]\-*)"
+    CHORDBassAndQualityPattern = r"(?P<Bass>[1-7]['|,]?)(?P<Quality>[^\]]*)"
+    BaseAndChordStrPattern = r"(\<(1|2|4|8|16|32)\*>)([^\<|$]+ )" # put a "<4*>[1][6m][4][5][1][5][1]-" to split tickBase and chorda string  
     pass
 #  [["6", "♯", "m"], "7-5", ["3", "♭"],  [1, 0.5]]  # means 6♯m7-5/3♭ (bass on 3,) with 1 bar before and place at 0.5 * bar_length
 #    Chord :[
@@ -96,3 +98,8 @@ def ChordStringGetter(PartsContainsChord):
 #            Position    -> [X, W]                                            #-> X bars after and print at the W * Bar_length (1>W>0)
 #            ]
 #
+# =========================================================================================
+# In [10]: [int(re.findall(r"(\<(1|2|4|8|16|32)\*>)([^\<|$]+)" , opc[3])[0][1]), re.findall(r"(\<(1|2|4|8|16|32)\*>)([^\<|$]+ )" , opc[3])[0][2]]
+# Out[10]: [4, '[1]-[1sus4][1maj7][3]-[3sus4][3][4]-[4/2][4][4m]-[6,][7,]']
+#In [11]: opc
+#Out[11]: ['Ending', 'CHORD', '|0|', '<4*>[1]-[1sus4][1maj7][3]-[3sus4][3][4]-[4/2][4][4m]-[6,][7,]<1*>[1]-[1/5]-[1]-[1][1]']
